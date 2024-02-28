2449f624fcc688818b65975efb6ae18baa47eb87
Gagan Randhawa <gagan23randhawa@gmail.com>
2024-02-28 17:35:15 +0530

    added pogo classes

---
+++ src/main/java/com/contactus/pojo/Admin.java
@@ -0,0 +1,20 @@
+package com.contactus.pojo;
+
+public class Admin {
+	
+	String name;
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	String password;
+
+}
---
+++ src/main/java/com/contactus/servlet/AddUser.java
@@ -0,0 +1,30 @@
+package com.contactus.servlet;
+
+import java.io.IOException;
+import com.contactus.*;
+import com.contactus.pojo.User;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+@WebServlet("/AddUser")
+public class AddUser extends HttpServlet {
+	
+	
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		   
+		User user = new User();
+		
+		user.setName ((request.getParameter("name")).toString());
+		user.setEmail ((request.getParameter("email")).toString());
+		user.setMessage ((request.getParameter("message")).toString());
+		
+		
+		
+	}
+
+}
