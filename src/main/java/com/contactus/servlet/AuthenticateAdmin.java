4abe239a2cecb7bfc9dcfd29d6eefd9489dd4876
Gagan Randhawa <gagan23randhawa@gmail.com>
2024-02-29 10:59:20 +0530

    Authentication

--- src/main/java/com/contactus/servlet/AuthenticateAdmin.java
+++ src/main/java/com/contactus/servlet/AuthenticateAdmin.java
@@ -8,6 +8,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import com.contactus.Dao.AdminDao;
 import com.contactus.Dao.UserDao;
 import com.contactus.pojo.User;
 
@@ -22,7 +23,20 @@ public class AuthenticateAdmin extends HttpServlet {
 		admin.setPassword ((request.getParameter("password")).toString());
 		
 		
-		
+		try {
+			if(AdminDao.validAdmin(admin))
+			{
+				response.sendRedirect("DisplayData.jsp");
+				System.out.println("authenticated admin");
+			}
+			else
+			{
+				response.sendRedirect("login.html");
+			}
+		} catch (ClassNotFoundException | IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 
 }
